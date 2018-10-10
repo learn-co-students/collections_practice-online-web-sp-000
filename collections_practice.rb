8cf0da1adb49f70ebe07326e86260cf12505ede4
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-10 18:10:48 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -59,9 +59,7 @@ end
 
 def add_s(array)
   array.map do |word|
-    if  word != array[1]
-      word << "s"
-    end
-          word
+    word << "s" if word != array[1]      
+    word
   end
 end
