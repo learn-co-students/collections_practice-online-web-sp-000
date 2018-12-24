3c74785946ec80f4cd317a432149fd3259ffbc60
Aglae Verduzco <aglae.cv@gmail.com>
2018-12-24 02:31:30 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,48 @@
+def sort_array_asc(array)
+  result = array.sort do |a,b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(array)
+  result = array.sort do |a, b|
+      b <=> a
+  end
+end
+
+def sort_array_char_count(num)
+  num.sort {|left, right| left.length <=> right.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |word|
+    word[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.select { |word| word.start_with?('a') }
+end
+
+def sum_array(array)
+  array.inject { |sum, n| sum + n}
+end
+
+def add_s(array)
+  array.each_with_index.collect do |element, index|
+    if index == 1
+    element
+  else
+    element + "s"
+  end
+end
+end
