67a0484c03e3432a2ba217219ec27adfb6704051
Diana Badas <diana.badas@gmail.com>
2018-12-27 16:57:27 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,23 @@
+def sort_array_asc (array)
+  array.sort
+end
 
+def sort_array_desc (array)
+  #array.sort.reverse
+  array.sort { |a,b| b <=> a }
+end
+
+def sort_array_char_count (array)
+  #array.sort_by(&:length)
+  array.sort { |a,b| a.length <=> b.length }
+end
+
+def swap_elements (array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def swap_elements_from_to (array, index, destination_index)
+  array[index], array[destination_index] = array[destination_index], array[index]
+  array
+end
