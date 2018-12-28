0eeda843e0c454e35177b50cfda8ac8e9725f38c
Diana Badas <diana.badas@gmail.com>
2018-12-28 00:48:22 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -21,3 +21,32 @@ def swap_elements_from_to (array, index, destination_index)
   array[index], array[destination_index] = array[destination_index], array[index]
   array
 end
+
+def reverse_array (array)
+  array.reverse
+end
+
+def kesha_maker (array)
+  arr = []
+  array.each do |string|
+    arr << string.gsub(string[2], "$")
+  end
+
+  arr
+end
+
+def find_a (array)
+  array.select { |str| str.start_with?("a") }
+
+end
+
+def sum_array (array)
+  array.inject(0) { |result, element| result + element }
+
+end
+
+def add_s (array)
+  array.each_with_index do |string, index|
+    string[string.length] = "s" unless index == 1
+  end
+end
