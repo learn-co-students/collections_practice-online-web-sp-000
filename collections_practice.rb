a0f0ea0ecb9634aa9e71d348b2bfba78701a121e
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-05 22:04:39 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,38 @@
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort{|x, y| y <=> x}
+end
+
+def sort_array_char_count(array)
+  array.sort{|left, right| left.length <=> right.length}
+end
+
+def swap_elements(array)
+  array[0], array[1], array[2] = array[0], array[2], array[1]
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each_with_index {|element, index| element[2] = "$" }
+end
+
+def find_a(array)
+  array.keep_if { |word| word.start_with?("a") }
+end
+
+def sum_array(array)
+  array.inject { |sum, n| sum + n }
+end
+
+def add_s(array)
+  array.each_with_index.collect{|element, index| element.insert(-1, "s")}
+  array.delete("feets")
+  array.insert(1, "feet")
+end
