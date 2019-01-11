69801d56290af4e40dbb9be76a0db625123b8156
thompson-ad <thompson_ad@outlook.com>
2019-01-11 15:27:06 +0000

    first commit

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,30 @@
 
+def sort_array_asc(array)
+     array.sort
+end
+
+def sort_array_desc(array)
+    array.sort do |a,b|
+        b <=> a
+    end
+end
+
+def sort_array_char_count(array)
+    array.sort do |a,b|
+        a.length <=> b.length
+    end
+end
+
+def swap_elements(array)
+    array.sort do |a,b|
+        if a == array[1] && b == array[2]
+             1
+        else
+            0
+        end
+    end
+end
+
+def 
+
+    
