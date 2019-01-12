defa7552f8243706776a0523a4427f05759a26c4
Aaron Thompson <thompson_ad@outlook.com>
2019-01-12 10:13:51 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,4 +1,3 @@
-
 def sort_array_asc(array)
      array.sort
 end
@@ -25,6 +24,40 @@ def swap_elements(array)
     end
 end
 
-def 
+def reverse_array(array)
+  array.reverse
+end
 
-    
+def kesha_maker(array)
+  new_array = []
+  array.each do |idx|
+    split = idx.chars
+    split[2] = "$"
+    new_array << split.join
+  end
+  new_array
+end
+
+def find_a(array)
+  new_array=[]
+  array.each do |word|
+    if (word.start_with?("a"))
+      new_array << word
+    end
+  end
+  new_array
+end
+
+def sum_array(array)
+  array.inject {|total, n| total + n}
+end
+
+def add_s(array)
+  array.each_with_index.collect do |element, idx|
+    if idx != 1
+      element + "s"
+    else
+      element
+    end
+  end
+end
