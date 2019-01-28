3d65ab50a0eba6d6f467d8c8c95de01202bc25d4
Lizzie Chan <lchan217@gmail.com>
2019-01-28 16:00:06 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,55 @@
+def sort_array_asc(array)
+  array.sort do |a,b|
+    a<=>b
+  end
+end
 
+def sort_array_desc(array)
+  array.sort do |a,b|
+    b<=>a
+  end
+end
+
+def sort_array_char_count(array)
+   array.sort do |a,b|
+    a.length<=>b.length
+  end
+end
+
+def swap_elements(array)
+  array.sort do |a,b|
+    a[1]<=>b[2]
+  end
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |x|
+   x[2] = "$"
+    end
+end
+
+def find_a(array)
+  array.select do |x|
+    x.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  array.inject { |sum, n| sum + n } 
+end 
+
+
+
+def add_s(array)
+    array.collect do |x|
+      if array[1] == x 
+        x
+      else 
+        "#{x}s"
+  end
+  end
+end 
