e3871549357163f77852be5f58a1b061886beaf3
Jared Grove <jaredkgrove@gmail.com>
2019-03-15 18:39:37 +0000

    Done.

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -29,6 +29,13 @@ describe 'collections practice' do
       expect(swap_elements(["blake", "ashley", "scott"])).to eq(["blake", "scott", "ashley"])
     end
   end
+  
+    # Question 4.1
+  describe '#swap_elements_from_to' do
+    it 'swap the elements of an array corresponding to the given indexes' do
+      expect(swap_elements_from_to(["blake", "ashley", "scott"],2,0)).to eq(["scott", "ashley", "blake"])
+    end
+  end
 
   # Question 5
   describe '#reverse_array' do
