715028ab81734a860a569c87a390885f39373703
Connor Cowan <ccowan003@comcast.net>
2019-06-11 21:10:27 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,28 @@
+def sort_array_asc(array)
+  array.sort { |a, b|
+    (a <=> b)
+  }
+  end
 
+def sort_array_desc(array)
+  array.sort { |a, b|
+    -(a <=> b)            #opposite of sorting with the '-' :)
+  }
+end
+
+def sort_array_char_count(array)
+  array.sort { |a, b|
+    (a.to_a <=> b.to_a)
+  }
+
+end
+
+def swap_elements(array)
+
+end
+
+def reverse_array(array)
+  array.map { |int|
+    int.reverse!
+  
+end
