345f355a01cdd5b20a9be70041bd038169c8bb29
David Strauch <dmaster18@gmail.com>
2019-07-30 20:37:38 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,29 @@
+number_array = [7,2, 1032, 5,19,4,202,8,3,1,-5]
+def sort_array_asc(number_array)
+  i = 0 
+  collection = []
+  while i < number_array.length
+    collection << yield(number_array[i])
+  end
+  collection
+end
 
+def sort_array_desc(number_array)
+  i = 0 
+  collection = []
+  while i < number_array.length
+    collection << yield(number_array[i])
+  end
+  collection.reverse
+end
+
+array.sort do |a, b|
+    a<=>b
+  end
+  
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a<=>b
+  end
+  array.reverse
+end
