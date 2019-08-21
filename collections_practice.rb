0d99f11548e8b34da0802ae27bce4cd406fa22cd
favorable-device-5158 <blee2125@gmail.com>
2019-08-21 19:53:15 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,31 +11,42 @@ def sort_array_desc(array)
 end
 
 def sort_array_char_count(array)
-  array.sort(length) do |a,b|
-    a<=>b
+  array.sort do |a,b|
+    a.length<=>b.length
   end
 end
 
 def swap_elements(array)
-
+  array[1],array[2]=array[2],array[1]
+  array
 end
 
 def reverse_array(array)
-
+  array.reverse
 end
 
 def kesha_maker(array)
-
+  array.each do |words|
+    words[2] = "$"
+  end
 end
 
 def find_a(array)
-
+  array.select do |words|
+    words.start_with? "a"
+  end
 end
 
 def sum_array(array)
-
+  array.inject(:+)
 end
 
 def add_s(array)
-
+  array.collect do |words|
+    if array[1] == words
+      words
+    else
+      words + "s"
+    end
+  end
 end
