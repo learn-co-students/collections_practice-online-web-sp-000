5f1edcced5c2dde51cfa59645eda5dfb0ee3f6a7
Michael Kofron <kofronmichael@gmail.com>
2019-08-21 17:21:10 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,49 @@
+def sort_array_asc(array)
+  new_array = array.sort
+  new_array
+end
 
+def sort_array_desc(array)
+  new_array = array.sort.reverse
+  new_array
+end
+
+def sort_array_char_count(array)
+  new_array = array.sort_by { |x| x.length }
+  new_array
+end
+
+def swap_elements(array)
+  array = array
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  new_array = []
+  array.each do |word|
+    word[2] =  "$"
+  end
+end
+  
+def find_a(array)
+  array.select { |word| word.start_with?('a') }
+end
+
+def sum_array(array)
+  array.inject(0){|sum,x| sum + x }
+end 
+    
+def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
+end
