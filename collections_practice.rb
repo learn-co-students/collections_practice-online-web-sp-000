ba958ddbda4a191c9fd266565c11d3681f5156e3
favorable-device-5158 <blee2125@gmail.com>
2019-08-21 18:35:14 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,41 @@
+def sort_array_asc(array)
+  array.sort do |a,b|
+    a<=>b
+  end
+end
 
+def sort_array_desc(array)
+  array.sort do |a,b|
+    b<=>a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort(length) do |a,b|
+    a<=>b
+  end
+end
+
+def swap_elements(array)
+
+end
+
+def reverse_array(array)
+
+end
+
+def kesha_maker(array)
+
+end
+
+def find_a(array)
+
+end
+
+def sum_array(array)
+
+end
+
+def add_s(array)
+
+end
