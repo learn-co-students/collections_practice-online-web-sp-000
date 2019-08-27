015176b13fda7b7895eadc742480be057577a612
Panela Leung <leungpanela@gmail.com>
2019-08-27 02:15:58 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,56 @@
+# sort integers in ascending order
+def sort_array_asc (array)
+  array.sort
+end
 
+def sort_array_desc(array)
+ array.sort.reverse
+end
+
+def sort_array_char_count (array)
+  sorted = array.sort_by {|x| x.length}
+end
+
+def swap_elements (array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def swap_elements_from_to(array, index, new_index)
+  array[index], array[new_index] = array[new_index], array[index]
+  return array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker (array)
+  array.each {|i| i[2] = "$"}
+end
+
+def find_a(array)
+  array.select {|i| i.start_with?("a")}
+end
+
+def sum_array(array)
+  array.inject {|sum, n| sum + n}
+end
+
+def add_s(array)
+  array.each_with_index.collect do |element, index|
+    #element + "s"
+
+end
+
+def add_s(array)
+  array.each_with_index.collect do |element, index|
+    if index != 1 #THE ELEMENT IS NOT THE SECOND
+    	element[index] + "s"
+	 else
+      	#what do we do here?
+        return element
+      end
+  end
+  end
+end
