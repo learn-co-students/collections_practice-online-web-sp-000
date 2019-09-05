0a3268ea792ecfedbb8543dfba6a8356a8e5820c
Tyler Landry <tyler.landry1996@outlook.com>
2019-09-05 14:58:27 -0700

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,42 @@
+def sort_array_asc(ascend)
+  ascend.sort
+end
 
+def sort_array_desc(descend)
+  descend.sort {|f,l| l <=> f}
+end
+
+def sort_array_char_count(counted)
+  counted.sort do |m, l|
+    m.length <=> l.length
+  end
+end
+
+def swap_elements(string)
+  string[1], string[2] = string[2], string[1]
+  string
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  string = array.each {|s| s[2] = "$"}
+  string
+end
+
+def find_a(array)
+  word = array.select {|s| s.start_with?("a")}
+  word
+end
+
+def sum_array(array)
+  array.inject(:+)
+end
+
+def add_s(array)
+  array.each_with_index do |s, i|
+    s << "s" unless i == 1
+  end
+end
