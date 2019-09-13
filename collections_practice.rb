3370355081088247bc91cd5e141549988af7348d
Blythe Pollard <blytheepollard@gmail.com>
2019-09-13 16:26:23 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,19 @@
+def sort_array_asc(integers)
+  integers.sort
+end  
 
+def sort_array_desc(integers)
+  integers.sort.reverse
+end  
+
+def sort_array_char_count(integers)
+  integers.sort {|a,b| a.length <=> b.length}
+end  
+
+def swap_elements(integers)
+  integers.sort {|a,b| a[1]<=>b[2]}
+end  
+
+def reverse_array(integers)
+  integers.reverse
+end  
