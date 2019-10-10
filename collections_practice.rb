8542690af8d261bc84eba9fbf8499eaefd0d0c11
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-10 00:53:37 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,12 +10,35 @@ def sort_array_desc(integer_array)
       b <=> a
   end
 end
-def sort_array_char_count(string_array)
 
-  string = string_array.to_s do
-  string.length
-  binding.pry
+def sort_array_char_count(string_array)
+    string_array.sort{|a, b| a.length <=> b.length}
 end
-  n_string_array =[]
-  n_string_array << string.sort{|a, b| a <=> b}
+
+def swap_elements(swap_array)
+swap_array[0], swap_array[1], swap_array[2] = swap_array[0], swap_array[2], swap_array[1]
+end
+
+def reverse_array(rev_arr)
+  rev_arr[0], rev_arr[1], rev_arr[2] = rev_arr[2], rev_arr[1], rev_arr[0]
+end
+
+# def kesha_maker(string_arr)
+#  yield (string_arr[3] = "$")
+# end
+# def my_each(array)
+#   i = 0
+#   while i < array.length
+#       yield (array[i])
+#       i = i + 1
+#   end
+#     array
+# end
+
+def find_a(strings)
+  strings.select{|letter| letter.start_with?("a")}
+end
+
+def sum_array(array)
+  array.inject(0){|sum,x| sum + x }
 end
