57eb3919bce29abe4fc45903f7f7fafce23a75d8
Erin  Rose <errose10@gmail.com>
2019-11-22 15:27:28 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -20,3 +20,33 @@ end
 def reverse_array(stuff)
   stuff.reverse
 end
+
+def kesha_maker(stuff)
+  stuff.each do |obj|
+  obj[2] = "$"
+  end
+end
+
+def find_a(stuff)
+  stuff.select do |obj|
+    obj[0] == "a"
+  end
+end
+
+def sum_array(stuff)
+  sum = 0
+  stuff.each do |num|
+    sum+=num
+  end
+  sum
+end
+
+def add_s(stuff)
+  stuff.collect do |obj|
+    if stuff[1] == obj
+      obj
+    else
+      obj + "s"
+    end
+  end
+end
