1ab190c380b692838ecdf5fd5e96799ae804b7c3
Erin  Rose <errose10@gmail.com>
2019-11-22 14:41:53 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,22 @@
+require 'pry'
 
+def sort_array_asc(stuff)
+  stuff.sort
+end
+
+def sort_array_desc(stuff)
+  stuff.sort.reverse
+end
+
+def sort_array_char_count(stuff)
+  stuff.sort_by { |obj| obj.length }
+end
+
+def swap_elements(stuff)
+  stuff[1], stuff[2] = stuff[2], stuff[1]
+  stuff
+end
+
+def reverse_array(stuff)
+  stuff.reverse
+end
