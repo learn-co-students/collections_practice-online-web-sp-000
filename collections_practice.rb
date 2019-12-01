c6c4b62bea837bf19417c85bebd71c74651b11ff
Sweet Van Loan <sweet@sweetvanloan.com>
2019-12-01 22:59:35 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,36 @@
+def sort_array_asc(array)
+  array.sort
+end 
 
+
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort_by {|element| element.length}
+end
+
+def swap_elements(array)
+  array.index(1,2) = array.index(2,1)
+end 
+
+def reverse_array(array)
+  array.sort.reverse
+end
+
+def kesha_maker(array)
+  array.each do {|element| element.2 = "$"}
+end
+
+def find_a(array)
+  array.start = "a"?
+end 
+
+def sum_array(array)
+  sum = array.reduce :+
+end
+
+def add_s(array)
+  array.each do {|element| element << "s"}
+end 
