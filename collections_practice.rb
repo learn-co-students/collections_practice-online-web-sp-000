1a06838bf987e2aaadd06c27185db4210f81caa5
Jennifer Pfeffer <jennifer.pfeffer127@gmail.com>
2020-01-28 17:58:14 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,29 @@
+def sort_array_asc(array) 
+  array.sort
+end 
+def sort_array_desc(array) 
+  array.sort.reverse
+end 
+def sort_array_char_count(array) 
+  array.sort.each do |string| 
+    string.lengtht
+end 
+def swap_elements(array) 
+
+end 
+def reverse_array(array) 
+  array.sort.reverse
+end 
+def kesha_maker(array) 
+  
+end 
+def find_a(array) 
+  
+end 
+def sum_array(array) 
+  
+end 
+def add_s(array) 
+  
+end 
 
