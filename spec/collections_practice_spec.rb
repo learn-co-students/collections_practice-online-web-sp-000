b5ddd8b07461119df4625d26352331a8445d5226
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-01-30 18:50:24 +0000

    Done.

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -30,6 +30,13 @@ describe 'collections practice' do
     end
   end
 
+# Question 4 advanced
+describe '#swap_elements_from_to' do
+    it 'swap elements in an array at a start and destination index' do
+      expect(swap_elements_from_to(["a", "b", "c"],0,2)).to eq(["c", "b", "a"])
+    end
+  end
+
   # Question 5
   describe '#reverse_array' do
     it 'reverse the order of an array of integers' do
