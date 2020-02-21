7adadb1132be33b4ec04f16ed06c38394c32d546
Alvin Coulson <aaacoulson@gmail.com>
2020-02-21 19:31:59 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,43 @@
 
+def sort_array_asc(array)
+  array.sort {|a, b| a <=> b }
+end
+
+def sort_array_desc(array)
+  array.sort {|a, b| b <=> a}
+end
+
+def sort_array_char_count(array)
+  array.sort {|a, b| a.length <=> b.length}
+end
+
+def swap_elements_from_to(array)
+  swap_elements_from_to(array, 1, 2)
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each {|name| name[2] = "$"}
+end
+
+def find_a(array)
+  array.select {|string| string.start_with?("a")}
+end
+
+def sum_array(array)
+  array.inject { |sum, n| sum + n }
+end
+
+def add_s(array)
+  array.each_with_index do |elem, index|
+    elem << "s" if index != 1
+  end
+end
