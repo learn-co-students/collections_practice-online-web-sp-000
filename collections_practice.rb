7c1e1ed540ab01801ac2de6f78ff48d126c6b2aa
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-03-10 19:46:39 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,37 +1,17 @@
 require 'pry'
 def sort_array_asc(array)
-  array.sort do |a,b|
-    if a == b
-      0
-    elsif a < b
-      -1
-    elsif a > b
-      1
-    end
-  end
+  array.sort
 end
 
 def sort_array_desc(array)
-  array.sort do |a,b|
-    if a == b
-      0
-    elsif a > b
-      -1
-    elsif a < b
-      1
-    end
+  array.sort do |a, b|
+    b <=> a
   end
 end
 
 def sort_array_char_count(array)
-  array.sort do |a,b|
-    if a.length == b.length
-      0
-    elsif a.length < b.length
-      -1
-    elsif a.length > b.length
-      1
-    end
+  array.sort do |a, b|
+    a.length <=> b.length
   end
 end
 
