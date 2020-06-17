e8ae17a1041ab471c29f0c96b316b625bfd628f5
reevesalic <reevesalic@gmail.com>
2020-06-17 17:27:12 -0400

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,17 @@
 
+def sort_array_asc(array)
+array = [25, 7, 1]
+array.sort
+end
+
+def sort_array_desc(array)
+array = [25, 7, 14]
+array.sort do|b, a|
+     a <=> b
+end
+end
+
+def sort_array_char_count(array)
+     array = [horses, cat, dogs]
+     array.sort.length
+end
