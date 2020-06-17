5f93ee4d68dc6dce3cfe2893770dd3eb51f85270
reevesalic <reevesalic@gmail.com>
2020-06-17 19:09:05 -0400

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -12,8 +12,14 @@ end
 end
 
 def sort_array_char_count(array)
-     array = [horses, cat, dogs]
+     array = ["Horses", "cat", "dogs"]
      array.sort do |a, b|
           a.length <=> b.length
      end  
+end
+
+def swap_elements(array)
+swap_elements = ["blake", "ashley", "scott"]    
+swap_elements_from_to(["a", "b", "c"],0,2) #=> ["c", "b", "a"]
+swap_elements_from_to(["a", "b", "c"],2,1) #=> ["a", "c", "b"]
 end
