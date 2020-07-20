01966e546610509d53c2f7e1eb0031d4606d726d
mirthful-concept-6030 <jchristinem@gmail.com>
2020-07-20 20:59:57 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,46 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a,b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    a.size <=> b.size
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.collect do |string|
+    strings_chars = string.chars
+    strings_chars[2] = "$"
+    strings_chars.join
+  end
+end
+
+def find_a(array)
+  array.select do |string|
+    string.start_with?("a","A")
+end
+end
+
+def sum_array(array)
+  array.inject(0){|sum,x| sum + x }
+end
+
+def add_s(array)
+  array.each_with_index.collect{|word,index| index != 1 ? word << "s" : word}
+end
