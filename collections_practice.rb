988b47fa186f065e982ffb779bc62530dfadb86e
Christerle Nounoute <christerlesn@gmail.com>
2020-09-16 04:21:00 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,44 @@
+def sort_array_asc(integers)
+  integers.sort
+end
 
+def sort_array_desc(integers)
+  integers.sort.reverse
+end
+
+def sort_array_char_count(integers)
+  integers.sort do |a, b|
+    a.size <=> b.size
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(integers)
+  integers.reverse
+end
+
+def kesha_maker(array)
+array.each {|i| i[2] = "$"}
+end
+
+def find_a(array)
+  array.select {|word| word.start_with? "a"}
+end
+
+def sum_array(array)
+  array.sum
+end
+
+def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
+end
