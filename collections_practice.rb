4e1c0046e37071a30ded85bf0ffd35aabece70e6
John Troutman <jtroutmanman@gmail.com>
2020-09-20 21:29:51 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,58 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  kesha_array = []
+  array.each do |word|
+    word[2] = "$"
+    kesha_array << word
+  end
+  kesha_array
+end
+
+def find_a(array)
+  a_array = []
+  array.each do |word|
+    if word.start_with?("a")
+      a_array << word
+    end
+  end
+  a_array
+end
+
+def sum_array(array)
+  array.inject(:+)
+end
+
+def add_s(array)
+  s_array = []
+  array.each_with_index do |word, i|
+    if i != 1
+      s_array << "#{word}s"
+    end
+  end
+  s_array.insert(1, array[1])
+  s_array
+end
