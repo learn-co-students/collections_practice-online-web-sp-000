1f7b7b061b9131c33fc677e85edee001b1185866
Mason Phillips <mjphillips2000@gmail.com>
2020-10-14 20:07:17 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,52 @@
+def sort_array_asc(num)
+  num.sort
+end
 
+def sort_array_desc(num)
+  num.sort.reverse
+end
+
+def sort_array_char_count(char)
+  char.sort{|x, y| x.length <=> y.length}
+end
+
+def swap_elements(nums)
+  temp = nums[1];
+	nums[1] = nums[nums.length-1];
+	nums[nums.length-1] = temp;
+	return nums;
+end
+
+def reverse_array(num)
+  num.reverse
+end
+
+def kesha_maker(char)
+  char.each do |x|
+    x[2] = "$"
+  end
+end
+
+def find_a(char)
+  char.find_all do |x|
+    x[0] == "a"
+  end
+end
+
+def sum_array(char)
+  sum = 0
+  char.each do |x|
+    sum += x
+  end
+  sum
+end
+
+def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
+end
