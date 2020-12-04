90eb896c1f50f2556c32fc5fc061d01265a4a0d8
Keith Filkins <kafilkins@me.com>
2020-12-04 23:47:39 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,53 @@
+def sort_array_asc(integers)
+  integers.sort do |a, b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(integers)
+  integers.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(characters)
+  characters.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[0], array[1], array[2] = array[0], array[2], array[1]
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  arrays = []
+  array.each do |word|
+    word_array = word.split ""
+    word_array[2] = "$"
+    arrays << word_array.join
+  end
+  arrays
+end
+
+def find_a(array)
+  array.select{|string| string.start_with?("a")}
+end
+
+def sum_array(integers)
+  integers.inject{|sum, n| sum + n}
+end
+
+def add_s(array)
+  array.each_with_index do |element, index|
+    if index == 1
+      element
+    else
+      element << "s"
+    end
+  end
+end
