0e343dfad3690e5790a0e8712e7622143982c4a1
Ben Hayes <b.t.hayes@comcast.net>
2020-12-15 20:24:13 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,62 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array [2] = array[2], array[1]
+  array
+end 
+
+def reverse_array(array)
+  new_array = array.reverse
+  new_array
+end
+  
+def kesha_maker(array)
+  new_array = []
+  array.each do |word|
+    word[2] = "$" 
+  new_array << word
+  end
+  new_array
+end
+
+def find_a(array)
+  array.select do |word|
+    word.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  array.inject(:+)
+end
+
+def add_s(array)
+  array.each_with_index.collect do |element, index|
+    if index != 1 
+      element << "s"
+    else 
+      element
+    end
+  end
+end
+
+
+  
+  
+  
+  
