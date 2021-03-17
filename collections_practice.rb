a6412304dbda5f4efb9a51ebc701c681e17ed7d7
Harrison Price <hprice518@gmail.com>
2021-03-17 02:04:49 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,5 +1,12 @@
 
 def sort_array_asc(sort_array)
   sort_array = [25, 7, 1]
-  sort_array.sort {|i| [0]<=>[1]<=>[2]}
+  sort_array.sort do |x,y|
+    if x == y
+      0 
+    elsif x > y 
+      1
+    elsif x < y 
+      -1
+    end  
 end
