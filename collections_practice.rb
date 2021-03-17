46f28e4a45d87fbdf32ebddd1a38d8c84eabcdc7
Harrison Price <hprice518@gmail.com>
2021-03-17 02:20:02 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,3 +11,31 @@ def sort_array_asc(array)
     end  
   end
 end
+
+def sort_array_desc(array)
+  array = [25, 7, 14]
+  array.sort do |x,y|
+    if x == y
+      0 
+    elsif x > y 
+      -1
+    elsif x < y 
+      1
+    end  
+  end
+end
+
+def sort_array_char_count(array)
+  array = ["dogs", "cat", "Horses"]
+  array.sort do |a,b|
+    if a == b
+      0 
+    elsif a > b 
+      1 
+    elsif a < b 
+      -1 
+    end 
+  end 
+end 
+
+
